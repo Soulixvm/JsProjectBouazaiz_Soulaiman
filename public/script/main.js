4114fcea16bfe004ff614c932f1958a455378210
Soulaiman <soulaiman.bouazaiz@hotmail.fr>
2022-06-17 13:26:53 +0200

    ajout disparition texte

--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,5 @@
 //script changement des points de vie en cliquant sur le bouton Attaque VIE
+//un bouton qui permet de changer la taille et le background d'une div ou section
 let attaque = document.getElementById("attaque");
 let vie = document.getElementById("pvActuelle");
 let mort = document.getElementsByClassName("salameche")[0];
@@ -14,30 +15,23 @@ attaque.addEventListener("click", function () {
     pvActuelle.style.backgroundColor = "#7f0000";
   } else {
     mort.style.filter = "grayscale()";
-    // alert("DEAD");
+  }
+});
+//En cliquant sur le bouton d'attaque texte il remplace le texte par un autre en faisant disparaitre le premier et en laissant apparaitre le deuxieme
+//lorsque l'on passe la souris sur certain textes elles doivent disparaître et faire place  à d'autres textes.
+let visible = document.getElementsByClassName("visible")[0];
+visible.style.display = "block";
+let invissible = document.getElementsByClassName("pInvisible")[0];
+invissible.style.display = "none";
+let boutonChgmntDeTexte = document.getElementsByClassName("attackTexte")[0];
+boutonChgmntDeTexte.addEventListener("click", () => {
+  if ((visible.style.display = "block")) {
+    visible.style.display = "none";
+    invissible.style.display = "";
+  } else {
+    visible.style.display = "block";
+    invissible.style.display = "none";
   }
 });
 
-// function Combat(sante) {
-//   if (pv > 0) {
-//     pv = pv - degat;
-//     pvActuelle.style.width = (pv / pvMAX) * 100 + "%";
-//     pvActuelle.style.backgroundColor = "#7f0000";
-//   } else {
-//     mort.style.filter = "grayscale()";
-//     // alert("DEAD");
-//   }
-// }
-//script qui hide et unhide le texte
-// pourquoi ça ne fonctionne pas
-function Disparait() {
-  let invisible = document.getElementsByTagName("pInvisible")[0];
-  let visible = document.getElementsByTagName("question")[0];
-  if (invisible.style.display = "none") {
-    invisible.style.display = "block";
-    visible.style.display = "none";
-  } else {
-    invisible.style.display = "none";
-    visible.style.display = "block";
-  }
-}
+
